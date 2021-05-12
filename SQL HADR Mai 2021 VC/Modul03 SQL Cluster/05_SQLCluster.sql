861f66c6ec263ce45d97a84335d44729597daeff
RauchendRaser <andreasr@ppedv.de>
2021-05-12 12:26:36 +0200

    SQL CLuster Installation

--- SQL HADR Mai 2021 VC/Modul03 SQL Cluster/05_SQLCluster.sql
+++ SQL HADR Mai 2021 VC/Modul03 SQL Cluster/05_SQLCluster.sql
@@ -14,6 +14,60 @@ Netzwerk für ServerClientKommunikation
 Tipp: auch die Netzwerkressource HDD auf das reine Servernetzwerk legen
 
 
+HDDs.. 
+
+Trenne Daten von Logs pro DB
+
+E:
+F:
+
+
+Z:
+
+
+Controller
+
+SQLA A SQLB P		SQLA P  SQLB A
+Knoten1			Knoten2
+16 Kerne	  16 Kerne
+256 GB RAM    256 GB RAM
+
+
+
+Clustered Shared Volumes
+
+E:  --> C:\CLusterStorage\Volume1 (Verknüpfung
+
+
+
+TempDB auch lokal sein
+
+der Pfad muss auch auf beiden Servern identisch angelegt sein...
+
+
+
+select * from sysdatabases
+
+
+SQLCLUST\PPEDV
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
